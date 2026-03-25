9857805766717947685692e5278ddfd5ca450100
verGuanlao <bguanlao@vertere-gs.com>
2026-03-25 14:33:56 +0800

    db: initialize MiniBankingSystem schema

---
+++ backend/schema.sql
@@ -0,0 +1,9 @@
+-- -----------------------------------------------------
+-- Schema MiniBankingSystem
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema MiniBankingSystem
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `MiniBankingSystem` DEFAULT CHARACTER SET utf8 ;
+USE `MiniBankingSystem` ;
